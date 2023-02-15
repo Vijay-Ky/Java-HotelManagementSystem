9d2d1da2e657dd1903383c23b24ef7e9db64f2f9
Vijay-Ky <vijayky007@gmail.com>
2023-02-15 15:37:25 +0530

    fourth_commit

--- HotelManagementSystem/src/com/hotel/Hotel.java
+++ HotelManagementSystem/src/com/hotel/Hotel.java
@@ -1,5 +1,10 @@
 package com.hotel;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -541,25 +546,133 @@ class Reception
 		}												
 	}
 }
+class Write implements Runnable
+{
+	Holder hotel_ob;
 	
+	Write(Holder hotel_ob)
+	{
+		this.hotel_ob = hotel_ob; 
+	}
 	
-	
-	
-	
-	
-
-
-
+	@Override
+	public void run()
+	{
+		try
+		{
+			FileOutputStream fout = new FileOutputStream("backup");
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			oos.writeObject(hotel_ob);
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Error in writing " + ex);
+		}
+	}
+}
 
 public class Hotel
 {
-
 	public static void main(String[] args)
 	{
-		
-
+		try
+		{
+			File file = new File("backup");
+			if(file.exists())
+			{
+				FileInputStream fin = new FileInputStream(file);
+				ObjectInputStream ois = new ObjectInputStream(fin);
+				Reception.hotel_ob = (Holder)ois.readObject();
+			}
+			Scanner sc = new Scanner(System.in);
+			int ch, ch2;
+			char wish;
+			loop:
+			do
+			{
+				System.out.println("\nEnter your choice :\n1.Display room details\n2.Display Room avilability \n3.Book\n4.Order food\n5.Checkout\n6.Exit\n");
+				ch = sc.nextInt();
+				switch(ch)
+				{
+					case 1:
+							System.out.println("\nChoose room type :\n1.Luxury Double Room \n2.Deluxe Double Room \3.Luxury Single Room \n4.Deluxe Single Room\n");
+						ch2 = sc.nextInt();
+						Reception.features(ch2);
+						break;
+					case 2:
+						System.out.println("\nChoose room type :\n1.Luxury Double Room \n2.Deluxe Double Room \3.Luxury Single Room \n4.Deluxe Single Room\n");
+						ch2 = sc.nextInt();
+						Reception.availability(ch2);
+						break;
+					case 3:
+							System.out.println("\nChoose room type :\n1.Luxury Double Room \n2.Deluxe Room \3.Luxury Single Room \n4.Deluxe Single Room\n");
+							ch2 = sc.nextInt();
+							Reception.bookroom(ch2);
+							break;
+					case 4:
+							System.out.println("Room Number - ");
+							ch2 = sc.nextInt();
+							if(ch2 > 60)
+								System.out.println("Room doesnt exist");
+							else if(ch2 > 40)
+								Reception.order(ch2 - 41, 4);
+							else if(ch2 > 30)
+								Reception.order(ch2 - 31, 3);
+							else if(ch2 > 10)
+								Reception.order(ch2 - 11, 2);
+							else if(ch2 > 0)
+								Reception.order(ch2 - 1, 1);
+							else
+								System.out.println("Room doesnt exist");
+							break;
+					case 5:
+						System.out.println("Room Number - ");
+						ch2 = sc.nextInt();
+						if(ch2 > 60)
+							System.out.println("Room doesnt exist");
+						else if(ch2 > 40)
+							Reception.deallocate(ch2 - 41, 4);
+						else if(ch2 > 30)
+							Reception.deallocate(ch2 - 31, 3);
+						else if(ch2 > 10)
+							Reception.deallocate(ch2 - 11, 2);
+						else if(ch2 > 0)
+							Reception.deallocate(ch2 - 1, 1);
+						else
+							System.out.println("Room doesnt exist");
+						break;
+					case 6: break loop;
+				}
+				System.out.println("\nContinue : (y/n)");
+				wish = sc.next().charAt(0);
+				if(!(wish == 'y' || wish == 'Y' || wish == 'n' || wish == 'N'))
+				{
+					System.out.println("Invalid Option");
+					System.out.println("\nContinue : (y/n)");
+					wish = sc.next().charAt(0);
+				}
+			}
+			while(wish == 'y' || wish == 'Y');
+			
+			Thread t1 = new Thread(new Write(Reception.hotel_ob));
+			t1.start();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Not a valid input");
+		}
 	}
-
 }
+
+	
+	
+	
+	
+	
+
+
+
+
+
 
 
